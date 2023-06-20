8848679161f6bed781dc9767088b12cbed356c6d
Man Yiu <11912398+manyiu@users.noreply.github.com>
2023-06-20 23:06:41 +0800

    Initial commit

---
+++ test/aws-cdk-frontend-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkFrontendDemo from '../lib/aws-cdk-frontend-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-frontend-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkFrontendDemo.AwsCdkFrontendDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
