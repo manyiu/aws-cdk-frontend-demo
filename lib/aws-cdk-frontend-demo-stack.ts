02b762a06a95f9bdbe36acd4fcdfab73f06f8c99
Man Yiu <11912398+manyiu@users.noreply.github.com>
2023-06-21 00:06:56 +0800

    init commit

--- lib/aws-cdk-frontend-demo-stack.ts
+++ lib/aws-cdk-frontend-demo-stack.ts
@@ -1,16 +1,68 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+require("dotenv").config();
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+const domainName = `${
+  process.env.GITHUB_HEAD_REF === "main"
+    ? ""
+    : process.env.GITHUB_HEAD_REF?.replace("/", "_") + "."
+}${process.env.GITHUB_REPOSITORY_ID}.${process.env.HOST_ZONE}`;
 
 export class AwsCdkFrontendDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hostingBucket = new cdk.aws_s3.Bucket(this, "HostingBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkFrontendDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const existingHostZone = cdk.aws_route53.HostedZone.fromLookup(
+      this,
+      "ExistingHostedZone",
+      {
+        domainName: process.env.HOST_ZONE as string,
+      }
+    );
+
+    const domainCertificate = new cdk.aws_certificatemanager.Certificate(
+      this,
+      "DomainCertificate",
+      {
+        domainName,
+        validation:
+          cdk.aws_certificatemanager.CertificateValidation.fromDns(
+            existingHostZone
+          ),
+      }
+    );
+
+    const distribution = new cdk.aws_cloudfront.Distribution(
+      this,
+      "Distribution",
+      {
+        defaultBehavior: {
+          origin: new cdk.aws_cloudfront_origins.S3Origin(hostingBucket),
+        },
+        domainNames: [domainName],
+        certificate: domainCertificate,
+      }
+    );
+
+    const cloudfrontTarget = new cdk.aws_route53_targets.CloudFrontTarget(
+      distribution
+    );
+
+    new cdk.aws_route53.ARecord(this, "AliasRecord", {
+      zone: existingHostZone,
+      recordName: domainName,
+      target: cdk.aws_route53.RecordTarget.fromAlias(cloudfrontTarget),
+    });
+
+    new cdk.aws_route53.AaaaRecord(this, "AliasRecord", {
+      zone: existingHostZone,
+      recordName: domainName,
+      target: cdk.aws_route53.RecordTarget.fromAlias(cloudfrontTarget),
+    });
   }
 }
