cd865be759d2121d3bc68c63f8843d73e222fd3c
Man Yiu <11912398+manyiu@users.noreply.github.com>
2023-06-27 13:55:53 +0800

    add cdk default account and region in env

--- bin/aws-cdk-frontend-demo.ts
+++ bin/aws-cdk-frontend-demo.ts
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AwsCdkFrontendDemoStack } from '../lib/aws-cdk-frontend-demo-stack';
+require("dotenv").config();
+import * as cdk from "aws-cdk-lib";
+import "source-map-support/register";
+import { AwsCdkFrontendDemoStack } from "../lib/aws-cdk-frontend-demo-stack";
 
 const app = new cdk.App();
-new AwsCdkFrontendDemoStack(app, 'AwsCdkFrontendDemoStack', {
+new AwsCdkFrontendDemoStack(app, "AwsCdkFrontendDemoStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
